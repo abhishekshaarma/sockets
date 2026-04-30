a6502eb420d4e8e32b474447673448924ce22e5f
Abhishek Sharma <98975746+abhishekshaarma@users.noreply.github.com>
2026-04-30 11:05:24 -0500

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -2,107 +2,54 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <string.h>
-#include "ecc.h"
 
 using namespace std;
 
-void showEncrypt(string msg, int key)
+int main()
 {
-    cout << "\n--- Encryption Process ---\n";
-    cout << "Plaintext: " << msg << "\n";
-    cout << "Key (shared secret x): " << key << "\n";
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    cout << "Ciphertext (ASCII values): ";
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8080);
+    addr.sin_addr.s_addr = INADDR_ANY;
 
-    for(char c : msg)
-    {
-        char enc = c ^ key;
-        cout << (int)enc << " ";
-    }
+    bind(server_fd, (sockaddr*)&addr, sizeof(addr));
+    listen(server_fd, 2);
 
-    cout << "\n--------------------------\n";
-}
+    cout << "[SERVER] Waiting for clients...\n";
 
-int main(int argc, char* argv[])
-{
-    string role = (argc > 1) ? argv[1] : "client";
+    int A = accept(server_fd, NULL, NULL);
+    cout << "[SERVER] Alice connected\n";
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    int B = accept(server_fd, NULL, NULL);
+    cout << "[SERVER] Bob connected\n";
 
-    sockaddr_in server;
-    server.sin_family = AF_INET;
-    server.sin_port = htons(8080);
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    int alicePub[2], bobPub[2];
 
-    connect(sock, (sockaddr*)&server, sizeof(server));
+    cout << "[SERVER] Receiving public keys...\n";
 
-    cout << "\n[" << role << "] Connected to server\n";
+    recv(A, alicePub, sizeof(alicePub), 0);
+    recv(B, bobPub, sizeof(bobPub), 0);
 
-    // STEP 1: private key
-    int priv;
-    cout << "Enter private key: ";
-    cin >> priv;
+    cout << "[SERVER] Exchanging keys...\n";
 
-    cout << "\n--- ECC KEY GENERATION ---\n";
-    cout << "Private key = " << priv << "\n";
+    send(A, bobPub, sizeof(bobPub), 0);
+    send(B, alicePub, sizeof(alicePub), 0);
 
-    Point G = {3, 6, false};
-    cout << "Base point G = (" << G.x << "," << G.y << ")\n";
+    cout << "[SERVER] Keys exchanged successfully\n";
 
-    // STEP 2: public key
-    Point pub = multiply(G, priv);
-    cout << "Public key = kG = (" << pub.x << "," << pub.y << ")\n";
+    char buffer[1024];
 
-    int pubArr[2] = {pub.x, pub.y};
-    send(sock, pubArr, sizeof(pubArr), 0);
+    recv(A, buffer, sizeof(buffer), 0);
+    cout << "[SERVER] Message received from Alice\n";
 
-    // STEP 3: receive other public key
-    int other[2];
-    recv(sock, other, sizeof(other), 0);
+    send(B, buffer, sizeof(buffer), 0);
+    cout << "[SERVER] Message forwarded to Bob\n";
 
-    Point otherPub = {other[0], other[1], false};
-    cout << "Received public key = (" << otherPub.x << "," << otherPub.y << ")\n";
+    close(A);
+    close(B);
+    close(server_fd);
 
-    // STEP 4: shared secret
-    Point shared = multiply(otherPub, priv);
-    cout << "Shared secret point = (" << shared.x << "," << shared.y << ")\n";
-
-    int key = shared.x;
-    cout << "Derived symmetric key = " << key << "\n";
-
-    cin.ignore();
-    string msg;
-
-    if(role == "alice")
-    {
-        cout << "\nEnter message: ";
-        getline(cin, msg);
-
-        showEncrypt(msg, key);
-
-        for(char &c : msg)
-            c ^= key;
-
-        send(sock, msg.c_str(), msg.size(), 0);
-
-        cout << "Encrypted message sent\n";
-    }
-    else
-    {
-        char buffer[1024];
-        recv(sock, buffer, sizeof(buffer), 0);
-
-        string received(buffer);
-
-        cout << "\nEncrypted received message (raw): " << received << "\n";
-
-        for(char &c : received)
-            c ^= key;
-
-        cout << "Decrypted message: " << received << "\n";
-    }
-
-    close(sock);
     return 0;
 }
