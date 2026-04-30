4fd70e0740004591af3365468a6dbf89c0b0950d
Abhishek Sharma <98975746+abhishekshaarma@users.noreply.github.com>
2026-04-30 11:10:51 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,109 +2,54 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include "ecc.h"
 
 using namespace std;
 
-int main(int argc, char* argv[])
+int main()
 {
-    string role = (argc > 1) ? argv[1] : "client";
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    cout << "[CLIENT] Started as " << role << "\n";
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8080);
+    addr.sin_addr.s_addr = INADDR_ANY;
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    bind(server_fd, (sockaddr*)&addr, sizeof(addr));
+    listen(server_fd, 2);
 
-    sockaddr_in server{};
-    server.sin_family = AF_INET;
-    server.sin_port = htons(8080);
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    cout << "[SERVER] Waiting for clients...\n";
 
-    cout << "[CLIENT] Connecting...\n";
+    int A = accept(server_fd, NULL, NULL);
+    cout << "[SERVER] Alice connected\n";
 
-    if(connect(sock, (sockaddr*)&server, sizeof(server)) < 0)
-    {
-        cout << "[ERROR] Connection failed\n";
-        return 0;
-    }
+    int B = accept(server_fd, NULL, NULL);
+    cout << "[SERVER] Bob connected\n";
 
-    cout << "[CLIENT] Connected to server\n";
+    int alicePub[2], bobPub[2];
 
-    // ---------------- PRIVATE KEY ----------------
-    int priv;
-    cout << "Enter private key: ";
-    cin >> priv;
+    cout << "[SERVER] Receiving public keys...\n";
 
-    cout << "[ECC] Private key = " << priv << "\n";
+    recv(A, alicePub, sizeof(alicePub), 0);
+    recv(B, bobPub, sizeof(bobPub), 0);
 
-    Point G = {3,6,false};
+    cout << "[SERVER] Exchanging keys...\n";
 
-    // ---------------- PUBLIC KEY ----------------
-    Point pub = multiply(G, priv);
-    cout << "[ECC] Public key = (" << pub.x << "," << pub.y << ")\n";
+    send(A, bobPub, sizeof(bobPub), 0);
+    send(B, alicePub, sizeof(alicePub), 0);
 
-    int pubArr[2] = {pub.x, pub.y};
+    cout << "[SERVER] Keys exchanged successfully\n";
 
-    cout << "[CLIENT] Sending public key...\n";
-    send(sock, pubArr, sizeof(pubArr), 0);
+    char buffer[1024];
 
-    // ---------------- RECEIVE KEY ----------------
-    int other[2];
+    recv(A, buffer, sizeof(buffer), 0);
+    cout << "[SERVER] Message received from Alice\n";
 
-    cout << "[CLIENT] Waiting for other public key...\n";
-    recv(sock, other, sizeof(other), 0);
+    send(B, buffer, sizeof(buffer), 0);
+    cout << "[SERVER] Message forwarded to Bob\n";
 
-    Point otherPub = {other[0], other[1], false};
+    close(A);
+    close(B);
+    close(server_fd);
 
-    cout << "[CLIENT] Received public key = ("
-         << otherPub.x << "," << otherPub.y << ")\n";
-
-    // ---------------- SHARED SECRET ----------------
-    Point shared = multiply(otherPub, priv);
-
-    cout << "[ECC] Shared secret = (" 
-         << shared.x << "," << shared.y << ")\n";
-
-    int key = shared.x;
-
-    cout << "[ECC] Derived key = " << key << "\n";
-
-    cin.ignore();
-
-    // ---------------- MESSAGE ----------------
-    if(role == "alice")
-    {
-        string msg;
-        cout << "Enter message: ";
-        getline(cin, msg);
-
-        cout << "[ENC] Original: " << msg << "\n";
-
-        for(char &c : msg)
-            c ^= key;
-
-        cout << "[CLIENT] Sending encrypted message...\n";
-
-        send(sock, msg.c_str(), msg.size(), 0);
-    }
-    else
-    {
-        char buffer[1024];
-
-        cout << "[CLIENT] Waiting for message...\n";
-        recv(sock, buffer, sizeof(buffer), 0);
-
-        string msg(buffer);
-
-        cout << "[ENC] Encrypted received: " << msg << "\n";
-
-        for(char &c : msg)
-            c ^= key;
-
-        cout << "[DEC] Decrypted message: " << msg << "\n";
-    }
-
-    close(sock);
-
-    cout << "[CLIENT] Done\n";
     return 0;
 }
